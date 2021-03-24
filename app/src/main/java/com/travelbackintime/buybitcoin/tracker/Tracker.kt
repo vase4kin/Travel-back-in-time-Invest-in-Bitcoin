d5f34f92356da4d56ae06f35533f51bf29b49fb3
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2021-03-24 15:53:10 +0800

    Add money suggestions tracking events (#29)

--- app/src/main/java/com/travelbackintime/buybitcoin/tracker/Tracker.kt
+++ app/src/main/java/com/travelbackintime/buybitcoin/tracker/Tracker.kt
@@ -34,11 +34,13 @@ private const val EVENT_USER_COPIES_BTC_WALLET = "event_user_copies_btc_wallet"
 private const val EVENT_USER_GETS_TO_SATOSHI = "event_user_gets_to_satoshi"
 private const val EVENT_USER_GETS_TO_EXIST = "event_user_gets_to_exist"
 private const val EVENT_USER_RETRIES = "event_user_retries"
+private const val EVENT_USER_CHOOSE_SUGGESTION = "event_user_choose_money_suggestion"
 private const val PARAMETER_TIME = "parameter_time"
 private const val PARAMETER_MONEY = "parameter_money"
 private const val PARAMETER_PROFIT = "parameter_profit"
 private const val PARAMETER_INVESTED = "parameter_invested"
 private const val PARAMETER_EVENT_NAME = "parameter_name"
+private const val PARAMETER_MONEY_SUGGESTION = "parameter_money_suggestion"
 
 interface Tracker {
 
@@ -71,6 +73,8 @@ interface Tracker {
     fun trackUserSharesOnTwitter()
 
     fun trackUserRetries()
+
+    fun trackUserChooseMoneySuggestion(amount: String)
 }
 
 class TrackerImpl(private val analytics: FirebaseAnalytics) : Tracker {
@@ -142,5 +146,11 @@ class TrackerImpl(private val analytics: FirebaseAnalytics) : Tracker {
 
     override fun trackUserSharesOnTwitter() {
         analytics.logEvent(EVENT_USER_SHARES_ON_TWITTER, null)
+    }
+
+    override fun trackUserChooseMoneySuggestion(amount: String) {
+        val bundle = Bundle()
+        bundle.putString(PARAMETER_MONEY_SUGGESTION, amount)
+        analytics.logEvent(EVENT_USER_CHOOSE_SUGGESTION, bundle)
     }
 }
