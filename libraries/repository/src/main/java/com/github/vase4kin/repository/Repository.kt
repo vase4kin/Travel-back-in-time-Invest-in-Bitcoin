4aa1ae3247ff1769b6d519f7929dcc68b12bc37b
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2021-03-29 07:13:58 +0800

    Libraries small improvements (#42)

--- libraries/repository/src/main/java/com/github/vase4kin/repository/Repository.kt
+++ libraries/repository/src/main/java/com/github/vase4kin/repository/Repository.kt
@@ -1,31 +1,8 @@
 package com.github.vase4kin.repository
 
-import com.github.vase4kin.coindesk.service.CoinDeskService
 import io.reactivex.Single
 
 interface Repository {
     fun getBitcoinPriceByDate(date: String): Single<Double>
     fun getCurrentBitcoinPrice(): Single<Double>
-}
-
-private const val USD = "USD"
-
-class RepositoryImpl(
-        private val coinDeskService: CoinDeskService
-) : Repository {
-
-    override fun getBitcoinPriceByDate(date: String): Single<Double> {
-        return coinDeskService.getBitcoinHistoricalPrice(date, date)
-                .map {
-                    it.bpi.values.firstOrNull()?.toDouble() ?: Double.NaN
-                }
-    }
-
-    override fun getCurrentBitcoinPrice(): Single<Double> {
-        return coinDeskService.getBitcoinCurrentPrice()
-                .map {
-                    it.bpi[USD]?.rate_float?.toDouble() ?: Double.NaN
-                }
-    }
-
 }
