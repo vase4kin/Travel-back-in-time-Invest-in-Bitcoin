92acd6cb609890d386f3a4b8e2870a769a93641e
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2021-04-15 13:29:35 +0800

    Prepare android release 1.1.3

--- buildSrc/src/main/kotlin/app/buildSrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/app/buildSrc/Dependencies.kt
@@ -20,8 +20,8 @@ object Config {
     const val compileSdkVersion = 30
     const val targetSdkVersion = 30
     const val buildToolsVersion = "30.0.2"
-    const val versionCode = 18
-    const val versionName = "1.1.2"
+    const val versionCode = 19
+    const val versionName = "1.1.3"
     const val applicationId = "com.travelbackintime.buybitcoin"
 }
 
